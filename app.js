f277ec7be9b32ee73e644c3f4772659ad47bb870
Fernando Leturia <f-leturia@hotmail.com>
2023-04-21 13:48:23 -0300

    -Avance en el curso

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const {
     inquirerMenu,
     pausa,
     leerInput,
+    listadoTareasBorrar,
 } = require("./helpers/inquirer")
 //const {mostrarMenu, pausa} = require("./helpers/mensajes")
 const Tareas = require('./models/tareas')
@@ -18,13 +19,10 @@ const main = async() => {
 
     const tareasDB = leerDB()
     if (tareasDB){
-
+        tareas.crearTareaFromArray(tareasDB)
     }
 
-    await pausa()
-
-    /*do {
-        //opt = await mostrarMenu()
+    do {
         opt = await inquirerMenu()
 
         switch (opt) {
@@ -34,26 +32,30 @@ const main = async() => {
                 break;
 
             case 2:
-                console.log(tareas.listadoArr)
+                tareas.listadoCompleto()
                 break;
 
             case 3:
+                tareas.listarPendientesCompletadas()
                 break;
 
             case 4:
+                tareas.listarPendientesCompletadas(false)
                 break;
 
             case 5:
                 break;
 
             case 6:
+                const id = await listadoTareasBorrar(tareas.listadoArr)
+                console.log({id})
                 break;
         }
 
         guardarDB(tareas.listadoArr)
 
         await pausa()
-    }while (opt !== '0')*/
+    }while (opt !== '0')
 }
 
 main()
--- models/tareas.js
+++ models/tareas.js
@@ -21,10 +21,59 @@ class Tareas {
         this._listado = {}
     }
 
+    crearTareaFromArray(tareas= []) {
+        tareas.forEach(tarea => {
+            this._listado[tarea.id] = tarea
+        })
+    }
+
     crearTarea(desc='') {
         const tarea = new Tarea(desc)
 
         this._listado[tarea.id] = tarea
+    }
+
+    listadoCompleto() {
+        console.log()
+
+        this.listadoArr.forEach((tarea, i) => {
+            const idx = `${i + 1}` .green
+            const {desc, completadoEn} = tarea
+            const estado = (completadoEn) ? 'Completada'.green : 'Pendiente'.red
+
+            console.log(`${idx} ${desc} :: ${estado}`)
+        })
+    }
+
+    listarPendientesCompletadas(completadas= true) {
+        console.log()
+
+        let indice = 0
+        this.listadoArr.forEach((tarea, i) => {
+            const idx = `${i + 1}` .green
+            const {desc, completadoEn} = tarea
+            const estado = (completadoEn) ? 'Completada'.green : 'Pendiente'.red
+
+            if (completadas){
+                //Completadas
+                if (completadoEn){
+                    indice += 1
+                    console.log(`${(indice + '.').green} ${desc} :: ${completadoEn}`)
+                }
+            }else {
+                //Pendientes
+                if (!completadoEn){
+                    indice += 1
+                    console.log(`${(indice + '.').red} ${desc} :: ${completadoEn}`)
+                }
+            }
+        })
+    }
+
+    borrar(id= '') {
+        if (this._listado[id]) {
+            delete this._listado[id]
+        }
     }
 }
 
